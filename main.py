53ea4c204fd5f515c2bcddb3ee5e5d752bc08d74
Noah Tsai <mrtsai@ucdavis.edu>
2023-09-27 15:57:20 -0700

    added dependencies and function to load wav file

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Hello World")
-#dataset origin: https://www.kaggle.com/datasets/kenjee/z-by-hp-unlocked-challenge-3-signal-processing/data
+import os
+from matplotlib import pyplot as plt
+import tensorflow as tf
+import tensorflow_io as tfio
+
+capuchin_sample = os.path.join('data','Parsed_Capuchinbird_Clips','XC3776-3.wav')
+not_capuchin_sample = os.path.join('data','Parsed_Not_Capuchinbird_Clips','afternoon-birds-song-in-fores-0.wav')
+
+print(not_capuchin_sample)
+
+def load_wav_16k_mono(filename):
+    file_contents = tf.io.read_file(filename)
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    wav = tf.squeeze(wav, axis=-1)
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
